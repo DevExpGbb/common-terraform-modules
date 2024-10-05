b97f345537af504f033b402f12389934a68df12d
Raymond Kao <raykao@github.com>
2024-10-05 21:50:08 +0000

    fix(azure_openai): using the wrong property name

--- azure_openai/azurerm_cognitive_account.tf
+++ azure_openai/azurerm_cognitive_account.tf
@@ -1,7 +1,7 @@
 resource "azurerm_cognitive_account" "default" {
   name                = local.name
   location            = var.resource_group.location
-  resource_group_name = var.resource_group.example.name
+  resource_group_name = var.resource_group.name
   kind                = var.kind
   sku_name            = var.sku_name
 }
