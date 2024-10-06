d2addd0fce2d2c9900234d6da6678e1b1c7cc18f
Raymond Kao <raykao@github.com>
2024-10-06 23:12:11 +0000

    fix: object() call is not needed here like in variables...thanks GHCP

--- aks/02-outputs.tf
+++ aks/02-outputs.tf
@@ -1,8 +1,8 @@
 output "cluster" {
-  value = object({
+  value = {
     id       = azurerm_kubernetes_cluster.default.id
     name     = azurerm_kubernetes_cluster.default.name
     cluster_principal_id = azurerm_user_assigned_identity.cluster.principal_id
     kubelet_principal_id = azurerm_user_assigned_identity.kubelet.principal_id
-  })
+  }
 }
