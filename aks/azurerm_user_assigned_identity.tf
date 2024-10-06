3ded164578b63bbd9e216851b6f8480360eb616c
Raymond Kao <raykao@github.com>
2024-10-06 23:08:18 +0000

    fix: forgot to ref the passed in RG

--- aks/azurerm_user_assigned_identity.tf
+++ aks/azurerm_user_assigned_identity.tf
@@ -1,13 +1,13 @@
 
 resource "azurerm_user_assigned_identity" "cluster" {
   name = "id-aks-runner"
-  resource_group_name = azurerm_resource_group.default.name
-  location = azurerm_resource_group.default.location
+  resource_group_name = var.resource_group.name
+  location = var.resource_group.location
 }
 
 
 resource "azurerm_user_assigned_identity" "kubelet" {
   name = "id-aks-runner-kubelet"
-  resource_group_name = azurerm_resource_group.default.name
-  location = azurerm_resource_group.default.location
+  resource_group_name = var.resource_group.name
+  location = var.resource_group.location
 }
