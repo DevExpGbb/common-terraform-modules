9a5fb0c85ec5cb3b9b6cef3bbf67de51a8dffc24
Raymond Kao <raykao@github.com>
2024-10-08 12:49:58 +0000

    chore(aks-msi): removing extraneous whitespace

--- aks/azurerm_user_assigned_identity.tf
+++ aks/azurerm_user_assigned_identity.tf
@@ -1,10 +1,8 @@
-
 resource "azurerm_user_assigned_identity" "cluster" {
   name = "${var.cluster_name}-cluster-id"
   resource_group_name = var.resource_group.name
   location = var.resource_group.location
 }
-
 
 resource "azurerm_user_assigned_identity" "kubelet" {
   name = "${var.cluster_name}-kubelet-id"
