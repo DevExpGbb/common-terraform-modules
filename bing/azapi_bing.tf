9b45a3875a0ff9ba8b27c39605e985a85817bf4e
Raymond Kao <raykao@github.com>
2024-10-08 23:42:06 +0000

    fix(bing): reference resource group variable

--- bing/azapi_bing.tf
+++ bing/azapi_bing.tf
@@ -3,7 +3,7 @@ resource "azapi_resource" "bing" {
   type                      = "Microsoft.Bing/accounts@2020-06-10"
   schema_validation_enabled = false
   name                      = var.name
-  parent_id                 = azurerm_resource_group.default.id
+  parent_id                 = var.resource_group.id
   location                  = "global"
   body = jsonencode({
     sku = {
