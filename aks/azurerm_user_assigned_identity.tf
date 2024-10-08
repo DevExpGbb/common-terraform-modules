61b6e6430c038ee2638d2b31e42891f4bbfa1082
Raymond Kao <raykao@github.com>
2024-10-08 12:16:27 +0000

    chore(aks): fix MSI naming

--- aks/azurerm_user_assigned_identity.tf
+++ aks/azurerm_user_assigned_identity.tf
@@ -1,13 +1,13 @@
 
 resource "azurerm_user_assigned_identity" "cluster" {
-  name = "id-aks-runner"
+  name = "${var.cluster_name}-cluster-id"
   resource_group_name = var.resource_group.name
   location = var.resource_group.location
 }
 
 
 resource "azurerm_user_assigned_identity" "kubelet" {
-  name = "id-aks-runner-kubelet"
+  name = "${var.cluster_name}-kubelet-id"
   resource_group_name = var.resource_group.name
   location = var.resource_group.location
 }
