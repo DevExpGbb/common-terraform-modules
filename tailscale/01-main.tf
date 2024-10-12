f60e43d90d14e0611eb26e689ab046a554de73f2
Raymond Kao <raykao@github.com>
2024-10-12 00:07:04 +0000

    fix(tailscale): remove old vars

--- tailscale/01-main.tf
+++ tailscale/01-main.tf
@@ -18,9 +18,6 @@ data "cloudinit_config" "cloudinit" {
   part {
     content_type = "text/x-shellscript"
     content = templatefile("${path.module}/scripts/tailscale.sh", {
-      # user_msi = var.user_assigned_identity_id
-      # secret_name = var.secret_name
-      # key_vault_name = var.key_vault_name
       tailscale_auth_key = var.tailscale_auth_key
       advertise_routes = var.advertise_routes
     })
